de977c09d7480c6289e3842d96d68b03bd51a6b8
Rizki Abdillah Azmi <rizkiabdillahazmi10@gmail.com>
2023-11-08 08:47:59 +0700

    add 04.string.go

---
+++ belajar-golang-dasar/04.string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Rizki")
+	fmt.Println("Rizki Abdillah")
+	fmt.Println("Rizki Abdillah Azmi")
+
+	fmt.Println(len("Rizki"))
+	fmt.Println("Rizki Abdillah"[0])
+	fmt.Println("Rizki Abdillah Azmi"[1])
+}
