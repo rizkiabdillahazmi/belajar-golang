3da11a70a152891ea7b6888831357f77ac06dc38
Rizki Abdillah Azmi <rizkiabdillahazmi10@gmail.com>
2023-11-09 15:00:16 +0700

    add 22.function_return_multiple_values.go

---
+++ belajar-golang-dasar/22.function_return_multiple_values.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func getFullName() (string, string) {
+	return "Rizki", "Abdillah"
+}
+
+func main() {
+
+	//firstName, lastName := getFullName()
+	//fmt.Println(firstName, lastName)
+
+	firstName, _ := getFullName()
+	fmt.Println(firstName)
+}
