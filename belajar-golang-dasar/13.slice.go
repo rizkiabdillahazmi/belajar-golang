b7eedc3c764d4487e7eee79c7fcbd9668d445580
Rizki Abdillah Azmi <rizkiabdillahazmi10@gmail.com>
2023-11-09 14:19:06 +0700

    add 13.slice.go

---
+++ belajar-golang-dasar/13.slice.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names = [...]string{
+		"Rizki",
+		"Abdillah",
+		"Azmi",
+		"Nizam",
+		"Salihin",
+	}
+
+	slice1 := names[1:3]
+	fmt.Println(slice1[0])
+	fmt.Println(slice1[1])
+	fmt.Println(slice1)
+
+	slice2 := names[:2]
+	fmt.Println(slice2)
+
+	slice3 := names[3:]
+	fmt.Println(slice3)
+
+	var slice4 []string = names[:]
+	fmt.Println(slice4)
+
+	days := [...]string{"Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu", "Minggu"}
+	daySlice1 := days[5:]
+	fmt.Println(daySlice1)
+
+	daySlice1[0] = "Sabtu Baru"
+	daySlice1[1] = "Minggu Baru"
+	fmt.Println(daySlice1)
+	fmt.Println(days)
+
+	daySlice2 := append(daySlice1, "Libur Baru")
+	daySlice2[0] = "Sabtu Lama"
+	fmt.Println(daySlice1)
+	fmt.Println(daySlice2)
+	fmt.Println(days)
+
+	newSlice := make([]string, 2, 5)
+	newSlice[0] = "Rizki"
+	newSlice[1] = "Abdillah"
+	//newSlice[2] = "Abdillah" error, harusnya pakai append
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	newSlice2 := append(newSlice, "Azmi")
+	fmt.Println(newSlice2)
+	fmt.Println(len(newSlice2))
+	fmt.Println(cap(newSlice2))
+
+	newSlice2[0] = "Budi"
+	fmt.Println(newSlice2)
+	fmt.Println(newSlice)
+
+	fromSlice := days[:]
+	toSlice := make([]string, len(fromSlice), cap(fromSlice))
+
+	copy(toSlice, fromSlice)
+	fmt.Println(fromSlice)
+	fmt.Println(toSlice)
+
+	// Notes
+	iniArray := [...]int{1, 2, 3, 4, 5}
+	iniSlice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(iniArray)
+	fmt.Println(iniSlice)
+}
