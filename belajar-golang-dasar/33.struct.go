339bdf0639be1e4d418b9e18e2fa75b3d54b54ab
Rizki Abdillah Azmi <rizkiabdillahazmi10@gmail.com>
2023-11-09 16:21:47 +0700

    add 33.struct.go

---
+++ belajar-golang-dasar/33.struct.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Biasanya nama attributnya pascal case
+type Customer struct {
+	Name, Address string
+	Age           int
+}
+
+func main() {
+	var rizki Customer
+	rizki.Name = "Rizki Abdillah Azmi"
+	rizki.Address = "Jakarta Pusat"
+	rizki.Age = 23
+
+	fmt.Println(rizki.Name)
+	fmt.Println(rizki.Address)
+	fmt.Println(rizki.Age)
+	fmt.Println(rizki)
+
+	nizam := Customer{
+		Name:    "Nizam Salihin",
+		Address: "Jakarta barat",
+		Age:     23,
+	}
+
+	fmt.Println(nizam)
+
+	joko := Customer{"Joko", "Bandung", 27}
+	fmt.Println(joko)
+}
