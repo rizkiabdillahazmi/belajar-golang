4b87d8badbe96a0ce0b2fbac3ea41b499fe9e569
Rizki Abdillah Azmi <rizkiabdillahazmi10@gmail.com>
2023-11-09 13:40:45 +0700

    add 10.perbandingan.go

---
+++ belajar-golang-dasar/10.perbandingan.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name1 = "Rizki"
+	var name2 = "Rizki"
+
+	var result bool = name1 == name2
+	fmt.Println(result)
+
+	var data1 = 10
+	var data2 = 5
+
+	var resultData = data1 < data2
+	fmt.Println(resultData)
+}
